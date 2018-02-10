40b2941577dc0ea1a734514bf08b3427f943a320
senbar <barsen97@gmail.com>
2018-02-10 10:18:53 -0800

    process age

--- processing.py
+++ processing.py
@@ -7,8 +7,17 @@ import visualisation as vis
 
 def processFeatures():
     train = ps.read_csv("data/train.csv")
-        
-    vis.Visualize(None);
+
+    #calculate mean age
+    ageAvr= train["Age"].mean()
+    ageStd= train["Age"].std()
+    ageNullCount = train['Age'].isnull().sum()
+    rndNullList = np.random.randint(ageAvr - ageStd, ageAvr + ageStd, size=ageNullCount)
+    
+    train['Age'][np.isnan(train['Age'])]= rndNullList
+    train['Age'] = train['Age'].astype(int)
+
+    vis.Visualize(train[["Age"]]);
 
 if __name__ == "__main__":
     processFeatures()
--- visualisation.py
+++ visualisation.py
@@ -2,11 +2,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def Visualize(data):
-    if(data==None):
+    if(data is None):
         return
 
-    g = sns.pairplot(data[[u'Survived', u'Pclass', u'Sex', u'Age', u'Parch', u'Fare', u'Embarked',
-                            ]], hue='Survived', palette='seismic', size=1.2, diag_kind='kde', diag_kws=dict(shade=True),
+    g = sns.pairplot(data, palette='seismic', size=1.2, diag_kind='kde', diag_kws=dict(shade=True),
                      plot_kws=dict(s=10))
     g.set(xticklabels=[])
     plt.show()
