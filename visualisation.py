f9d2d354f149410af44869ab93c6bd4bec8a6c93
MacenDi <mstanekofficial@gmail.com>
2018-02-10 21:34:39 +0100

    Heatplot added

--- visualisation.py
+++ visualisation.py
@@ -5,7 +5,13 @@ def Visualize(data,hueKey=None):
     if(data is None):
         return
 
-    g = sns.pairplot(data,hue=hueKey, palette='seismic', size=1.2, diag_kind='kde', diag_kws=dict(shade=True),
-                     plot_kws=dict(s=10))
+    g = sns.pairplot(data, hue=hueKey, palette='seismic', size=1.2)
     g.set(xticklabels=[])
+
+    colormap = plt.cm
+
+    plt.figure(figsize=(14,12))
+    plt.title('Correlation of features', y = 1.05, size = 15)
+    sns.heatmap(data.astype(float).corr(), linewidths=0.1, vmax=1,square=True,linecolor='white', annot=True)
+
     plt.show()
