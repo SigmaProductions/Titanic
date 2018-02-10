46d19dae69930642ce04ea7c05ef42efb927376d
MacenDi <mstanekofficial@gmail.com>
2018-02-10 19:30:16 +0100

    processing cabin, sex and Embarked

--- processing.py
+++ processing.py
@@ -4,11 +4,17 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import visualisation as vis
+import random
 
 def processFeatures():
     train = ps.read_csv("data/train.csv")
-        
-    vis.Visualize(None);
+    train['Sex'] = train['Sex'].map({'female': 0, 'male': 1}).astype(int)
+    train['Embarked'] = train['Embarked'].map({'S': 0, 'C': 1, 'Q': 2 , None:random.randint(-1,2)}).astype(int)
+    train['Cabin'] = train['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
+    dropElements = ['PassengerId', 'Name', 'Ticket']
+    train =  train.drop(dropElements, axis=1)
+    print(train.head(10))
+    #vis.Visualize(None);
 
 if __name__ == "__main__":
     processFeatures()
