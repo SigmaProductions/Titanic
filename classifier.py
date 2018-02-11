36c4673478f061f4521f69c1a32dd72901b63eb1
senbar <barsen97@gmail.com>
2018-02-10 18:52:16 -0800

    move into thinking in nodes, finish J function

--- classifier.py
+++ classifier.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-def learn(trainingSet):
+"""expects data frame"""
+def learn(training_frame):
 
     # Parameters
     learning_rate = 0.01
@@ -13,19 +14,27 @@ def learn(trainingSet):
     #x = tf.placeholder( [None, 7])  # mnist data image of shape 28*28=784
     #y = tf.placeholder( [None, 1])
 
+    training_array= training_frame.values.transpose()
+    n = training_array.shape[0]-1
+    m = training_array.shape[1]
 
+    #y=training_array[0,:].astype(np.float32)
+    y=tf.placeholder(tf.float32,(m))
+    X = tf.placeholder(tf.float32, (1+n, m))
 
-    n= len(trainingSet.keys())
-    m= len(trainingSet[trainingSet.keys()[0]])
-
-    # Set model weights
-    trainngSetWithBias= tf.concat([tf.ones([1,trainingSet]),trainingSet],1)
-    W = tf.Variable(tf.zeros([n, 1]))
+    W = tf.Variable(tf.zeros([1, n]))
     b = tf.Variable(tf.zeros([1,1]))
 
-    Theta=tf.concat([W, b], 0)
+    # add bias
+    #X = tf.concat([tf.ones([1, m]), X], 0)
+    Theta=tf.concat([b,W], 1)
+    h_of_x= tf.einsum('ij,ki->j', X,Theta )
 
-    pred = tf.nn.softmax(tf.matmul(trainngSetWithBias,Theta ))  # Softmax
-    h= tf.sigmoid(pred)
+    pred = tf.nn.sigmoid(h_of_x)  # Softmax
+
+    #J = 1 / m * sum( -1 * y' * log(h_of_x) - (1-y') * log(1 - h_of_x) );
+    J= 1/m * tf.reduce_sum(
+        tf.einsum("i,i->",-1*tf.transpose(y), tf.log(h_of_x))
+        - tf.einsum("i,i->",1-tf.transpose(y), tf.log(1- h_of_x)) )
 
 
